aa3366f8afaadabf7ac66ec7e07867458ee98b2f
Chad Chaney <cchane4@gmail.com>
2019-11-03 13:52:14 -0600

    adding the schema and model

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ const mongoose = require("mongoose");
 const express = require ('express');  
 app = express (); 
 
+//app config
 mongoose.connect("mongodb://localhost:27017/restful_blog_app", { useNewUrlParser: true, useUnifiedTopology: true }); 
 app.set("view engine", "ejs"); 
 app.use(express.static("public")); 
 app.use(body_parser.urlencoded({extended: true})); 
- 
+// Mongoose/model config
+let blog_schema = new mongoose.Schema({ 
+    title: String, 
+    image: String, 
+    body: String, 
+    created: { type: Date, default: Date.now}
+})
+
+let Blog = mongoose.model("Blog", blog_schema); 
+//restful routes
+
 
 app.listen(3000, () => { 
     console.log("yelp server has started"); 
